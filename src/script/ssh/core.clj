74fb5266e30afe25d56372bcc23d47f93ec80e4e
Martin Liu <hualiu@ebay.com>
2015-05-12 18:03:29 +0800

    use vector for ssh servers to keey order

--- src/script/ssh/core.clj
+++ src/script/ssh/core.clj
@@ -9,8 +9,8 @@
   "List all ssh servers"
   [& args]
   (let [len (apply max
-                   (map #(count (name %)) (keys @conf)))]
-    (println (->> @conf
+                   (map #(count (name %)) (keys (apply hash-map @conf))))]
+    (println (->> (partition 2 @conf)
                   (map (fn [[key value :as entry]]
                          (format (str "%" len "s" ": %s") (name key) value)))
                   (string/join \newline)))))
@@ -18,11 +18,11 @@
 (defn g
   "Get IP of a server"
   [key]
-  (println ((keyword key) @conf)))
+  (println ((keyword key) (apply hash-map @conf))))
 
 (defn s
   "SSH to a server"
   [key & args]
   (let [username (or (first args) "$(whoami)")
-        ip (or ((keyword key) @conf) key)]
+        ip (or ((keyword key) (apply hash-map @conf)) key)]
     (util/exec (str "ssh " username "@" ip))))
