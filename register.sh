f6e936b6820883b5c593fcef26ff68ee20859fd8
Martin Liu <hflh1989@gmail.com>
2015-08-19 10:14:24 +0800

    add boot2docker shellinit

--- register.sh
+++ register.sh
@@ -1,12 +1,20 @@
 ### Get current dir
 DIR=$( cd "$( dirname "$0" )" && pwd )
 export PATH=$PATH:$DIR/bin
+export TERM=screen-256color
 
 ## Add percol functions
 source $DIR/percol/percol.rc
 
 ## The fuck
 alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
+
+function exists { which $1 &> /dev/null }
+
+## Boot2docker
+if exists boot2docker; then
+    eval "$(boot2docker shellinit)"
+fi
 
 ### Martin's script
 
