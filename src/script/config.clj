3a5a22b7ff83c6a374d021c758ddc229b93b5193
Martin Liu <hualiu@ebay.com>
2016-03-02 18:32:12 +0800

    update config path

--- src/script/config.clj
+++ src/script/config.clj
@@ -1,7 +1,7 @@
 (ns script.config
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]))
-(def ^:const conf-file "/etc/config.edn")
+(def ^:const conf-file (str (System/getProperty "user.home") "/.config.edn"))
 (def config
   (delay (edn/read-string
           (slurp (or
