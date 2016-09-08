24cc7dd18f00a093f7b9dcd9bd753a546d1f12c1
Martin Liu <hualiu@ebay.com>
2016-09-07 21:40:16 -0700

    refine emacs function

--- register.sh
+++ register.sh
@@ -3,13 +3,10 @@ DIR=$( cd "$( dirname "$0" )" && pwd )
 export PATH=$PATH:$DIR/bin
 export TERM=screen-256color
 
-if [ ! $EMACS ]; then
-    # not in emacs
-    ZSH_THEME=agnoster
-else
-    # in emacs
-    ZSH_THEME=robbyrussell
-fi
+## Languages
+### GO
+export GOPATH=~/.go
+export PATH=$PATH:$GOPATH/bin
 
 ## Add percol functions
 source $DIR/shell/percol/percol.rc
@@ -21,23 +18,47 @@ function exists { which $1 &> /dev/null }
 
 ## docker for mac
 if exists docker-machine; then
-    command="docker-machine env vm 2> /dev/null"
+    command="docker-machine env default 2> /dev/null"
 
-    if [ $(docker-machine status vm) != "Running" ]; then
-        echo "Starting docker machine 'vm' in background..."
-        docker-machine start vm > /dev/null 2>&1 &
+    if [ $(docker-machine status default) != "Running" ]; then
+        echo "Starting docker machine 'default' in background..."
+        docker-machine start default > /dev/null 2>&1 &
     fi
 
     eval $(eval $command)
 
 fi
 
-### Martin's script
+## hub
+if exists hub; then
+    alias git=hub
+fi
+
+## emacs
 
 e(){
-    emacsclient $1 &
+    visible_frames() {
+        emacsclient -a "" -e '(length (visible-frame-list))'
+    }
+
+    change_focus() {
+        emacsclient -n -e "(select-frame-set-input-focus (selected-frame))" > /dev/null
+    }
+
+    # try switching to the frame incase it is just minimized
+    # will start a server if not running
+    test "$(visible_frames)" -eq "1" && change_focus
+
+    if [ "$(visible_frames)" -lt  "2" ]; then # need to create a frame
+        # -c $@ with no args just opens the scratch buffer
+        emacsclient -n -c "$@" && change_focus
+    else # there is already a visible frame besides the daemon, so
+        change_focus
+        # -n $@ errors if there are no args
+        test  "$#" -ne "0" && emacsclient -n "$@"
+    fi
 }
 
 et(){
-    emacsclient -t $1
+    exec emacsclient -a "" -t "$@"
 }
