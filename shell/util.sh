bd373a45fefb562acb615350ad35cc4055129365
Martin Liu <hualiu@ebay.com>
2017-03-28 17:33:31 -0700

    refine code

--- shell/util.sh
+++ shell/util.sh
@@ -2,6 +2,8 @@ function m_add_ssh_key() {
     cat ~/.ssh/id_rsa.pub | ssh $1 'cat >> /tmp/1.txt && mkdir -p .ssh && cat /tmp/1.txt >> .ssh/authorized_keys'
 }
 
+# Grant user access && sudo to a c3 server
+# Usage: `m_grant_user_sudo_access SERVER USER`
 function m_grant_user_sudo_access() {
     server=$1
     usr=$2
